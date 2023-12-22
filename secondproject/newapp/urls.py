95ca9df537ff5eb74d51da57ad5cc274d7567d98
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2023-12-22 11:35:38 -0600

    Updated Several files

--- secondproject/newapp/urls.py
+++ secondproject/newapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('kitchen/menu_details/<int:pk>', views.menu_details, name='menu_details'), 
     path('about/', views.about, name="about"),   
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
