aec93cd90a76413ab3ae1163bc783ae1aa946250
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2024-05-15 05:55:32 -0500

    Updated settings.py

--- secondproject/secondproject/settings.py
+++ secondproject/secondproject/settings.py
@@ -78,22 +78,27 @@ WSGI_APPLICATION = 'secondproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
+if DEBUG:
+    # SQLITE Confuguration
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'test',
         'USER': 'isotopex',
         'PASSWORD': 'isotopex',
-        
+
     }
 }
+    
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
