28dd635224fba1d91a61b2c7d18e8ecf399f42eb
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2025-07-10 14:33:54 -0500

    Updated urls.py

--- secondproject/newapp/urls.py
+++ secondproject/newapp/urls.py
@@ -11,7 +11,13 @@ urlpatterns = [
     path('kitchen/menu_details/<int:item_id>/', views.menu_details, name='menu_details'),
     path('place_order/<int:item_id>/', views.place_order, name='place_order'), 
     path('order_success/', views.order_success, name='order_success'),
-    path('about/', views.about, name="about"),   
+    path('about/', views.about, name="about"),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('cart/add/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/update_quantity/<int:item_id>/', views.update_quantity, name='update_quantity'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('submit_order/', views.submit_order, name='submit_order'),       
 ]
 
 if settings.DEBUG:
